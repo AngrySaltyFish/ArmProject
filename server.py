0d04243fe09f58ea47aea6f5adb34c46f645b999
Archangel7777777 <james.rand1@aol.co.uk>
2016-11-29 21:27:32 +0000

    This is a basic setup for the do_POST method
    This basically allows post methods to the server and displays the
    homepage instead of causing a connection error. Due to displaying
    index.html in both the do_POST and do_GET it has been extracted into
    a function to avoid code repition.

--- server.py
+++ server.py
@@ -5,14 +5,28 @@ import ssl
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        if self.path == '/jsonDataFile.json':
+            with open('jsonDataFile.json', 'rb') as file:
+                self.send_response(200)
+                self.send_header('Content-type', 'json')
+                self.end_headers()
+                self.wfile.write(file.read())
+        else:
+            self.homepage()
+
+    def do_POST(self):
+        self.send_response(200)
+        data = self.rfile.read()
+
+        self.homepage()
+
+    def homepage(self):
         with open('index.html', 'rb') as file:
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(file.read())
 
-    def do_POST(self):
-        pass
 
 
 if __name__ == "__main__":
