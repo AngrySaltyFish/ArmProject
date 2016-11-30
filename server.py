b75f83f0847bb5e849124104307a720f35a1a319
Archangel7777777 <james.rand1@aol.co.uk>
2016-11-30 17:53:15 +0000

    Adds access to database for server
    This allows the server to be able to add values to the database throught
    a post request which sends json data to the server.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from http.server import *
 import socketserver
 import ssl
+import json
+import sqlite3
 
 class Handler(BaseHTTPRequestHandler):
 
@@ -16,7 +18,9 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         self.send_response(200)
-        data = self.rfile.read()
+        length = int(self.headers["content-length"])
+        data = self.rfile.read(length)
+        database.insertValues(data)
 
         self.homepage()
 
@@ -27,9 +31,46 @@ class Handler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(file.read())
 
+class CreateDatabase():
+    def __init__(self):
+        self.dbinit()
 
+    def dbinit(self):
+        self.conn = sqlite3.connect("database.db")
+        self.c = self.conn.cursor()
+
+        self.c.execute("""
+        CREATE TABLE IF NOT EXISTS data (
+            lat int(4),
+            lng int(4),
+            C02 int(1),
+            C0 int(1)
+        )
+        """)
+
+    def insertValues(self, rawData):
+        #data = json.dumps(rawData)
+
+        try:
+            str = rawData.decode()
+            data = json.loads(str[str.find("["):str.rfind("]")+1])
+
+            for item in data:
+                self.c.execute("""
+                INSERT INTO data VALUES (
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                )
+                """, (item["location"][0], item["location"][1], item["C02"], item["C0"]))
+            self.conn.commit()
+
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
+    database = CreateDatabase()
     server = socketserver.TCPServer(('', 80), Handler)
     server.socket = ssl.wrap_socket (server.socket, certfile='cert.pem', keyfile='key.pem', server_side=True)
 
